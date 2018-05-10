f1474f4fe41df07c30cc99a583a4052df97e9092
Ricardo van Zutphen <ricardo@cuckoo.sh>
2018-05-10 04:28:44 +0200

    Add command/tool abilities to verify servers

--- socks5man/main.py
+++ socks5man/main.py
@@ -1,54 +1,19 @@
-from socks5man.manager import Manager
-import logging
+import click
+import sys
 
-logging.basicConfig(level=logging.DEBUG)
+from socks5man.tools import verify_all
 
-if __name__ == "__main__":
-    m = Manager()
+@click.group()
+@click.option("-d", "--debug", is_flag=True, help="Enable debug logging")
+def main(debug):
+    print "main app"
 
-    # b = [
-    #     {
-    #         "host":"192.168.1.1",
-    #         "port": 8231
-    #     },
-    #     {
-    #         "host": "cuckoo.rznet.nl",
-    #         "port": 12802,
-    #         "password": "Hallo",
-    #         "username": "Doei"
-    #     },
-    #     {
-    #         "host": "security.nl",
-    #         "port": 8231
-    #     },
-    #     {
-    #         "host": "115.132.114.252",
-    #         "port": 8181,
-    #         "description": "Very koel socks"
-    #     }
-    # ]
-    #m.bulk_add(b, description="Dag")
-
-    p = []
-
-    # for prox in a.split("\n"):
-    #     ip, port = prox.split(":")
-    #     p.append({
-    #         "host": ip,
-    #         "port": int(port)
-    #     })
-    #
-    # m.bulk_add(p)
-    import win_inet_pton
-    from socks5man.database import Database
-    from socks5man.socks5 import Socks5
-    l = Database().list_socks5()
-    print l
-
-    for s in l:
-        print "Trying: %s" % s.host
-        try:
-            if Socks5(s).verify():
-                print "Working: %s" % s
-        except Exception:
-            pass
+@main.command()
+@click.option("-s", "--service", is_flag=True, help="Start a service that keeps verifying all servers at the interval specified in the config")
+def verify(service):
+    """Verify if the servers are operational"""
+    try:
+        verify_all(service)
+    except KeyboardInterrupt:
+        print "CTRL+C detected! exiting."
+        sys.exit(0)
---
+++ socks5man/tools.py
@@ -0,0 +1,65 @@
+import logging
+import time
+
+from socks5man.config import cfg
+from socks5man.database import Database
+from socks5man.socks5 import Socks5
+
+log = logging.getLogger(__name__)
+db = Database()
+
+def verify_all(service=False):
+
+    last_bandwidth = None
+    bandwidth_checked = False
+    while True:
+        for socks5 in db.list_socks5():
+            socks5 = Socks5(socks5)
+
+            log.info(
+                "Testing socks5 server: '%s:%s'", socks5.host, socks5.port
+            )
+            if socks5.verify():
+                log.debug("Operationality check: OK")
+            else:
+                log.warning(
+                    "Operationality check (%s:%s): FAILED",
+                    socks5.host, socks5.port
+                )
+
+            if cfg("connection_time", "enabled"):
+                con_time = socks5.measure_connection_time()
+                if con_time:
+                    log.debug("Measured connection time: %s", con_time)
+                else:
+                    log.warning(
+                        "Connection time measurement failed for: '%s:%s'",
+                        socks5.host, socks5.port
+                    )
+
+            if cfg("bandwidth", "enabled"):
+                if last_bandwidth:
+                    waited = time.time() - last_bandwidth
+                    if waited < cfg("socks5man", "bandwidth_interval"):
+                        continue
+
+                bandwidth_checked = True
+                bandwidth = socks5.approx_bandwidth()
+                if bandwidth:
+                    log.debug(
+                        "Approximate bandwidth: %s Mbit/s down", bandwidth
+                    )
+                else:
+                    log.warning(
+                        "Bandwidth approximation test failed for: '%s:%s'",
+                        socks5.host, socks5.port
+                    )
+
+        if bandwidth_checked:
+            bandwidth_checked = False
+            last_bandwidth = time.time()
+
+        if not service:
+            break
+
+        time.sleep(cfg("socks5man", "verify_interval"))
