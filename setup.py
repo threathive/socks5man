625d3d644b2a3807f179f0c4e2cb47a5572b19a7
Ricardo van Zutphen <ricardo@cuckoo.sh>
2018-06-28 00:15:01 +0200

    Update setup info

--- setup.py
+++ setup.py
@@ -1,19 +1,48 @@
+import sys
+
 from setuptools import setup
+
+if sys.version[0] != "2":
+    sys.exit(
+        "Socks5man currently only supports Python 2.7. 3.5+ is on the roadmap"
+        ", but is not supported yet. For now, please install it in the"
+        " following way: `pip2 install -U socks5man`."
+    )
 
 setup(
     name="Socks5man",
-    version="0.1",
+    version="0.1.0",
     author="Ricardo van Zutphen",
+    author_email="ricardo@cuckoo.sh",
     packages=[
         "socks5man",
     ],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Environment :: Console",
+        "Framework :: Pytest",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Information Technology",
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: System Administrators",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Natural Language :: English",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: System :: Systems Administration",
+        "Topic :: System :: Monitoring",
+        "Topic :: Security",
+    ],
     license="GPLv3",
     description="SOCKS5 server management tool and library",
+    long_description=open("README.rst", "rb").read(),
     include_package_data=True,
+    url="https://socks5man.readthedocs.io",
     install_requires=[
         "PySocks==1.5.7",
         "geoip2==2.8.0",
-        "SQLAlchemy==1.2.7",
+        "SQLAlchemy>=1.0.8, <=1.2.7",
         "click==6.6"
     ],
     python_requires=">=2.7, <3.0",
@@ -22,6 +51,10 @@ setup(
             "win-inet-pton==1.0.1",
         ],
     },
+    tests_require=[
+        "pytest",
+        "mock"
+    ],
     entry_points={
         "console_scripts": [
             "socks5man = socks5man.main:main"
